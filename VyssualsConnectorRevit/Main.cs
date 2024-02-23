fde4f7230a7c9e4d3430210c3d34a0c09982781d
Yskert Schindel <yskert.schindel@gmail.com>
2024-02-23 17:07:57 +0100

    Setup Project

---
+++ VyssualsConnectorRevit/Main.cs
@@ -0,0 +1,21 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VyssualsConnectorRevit
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Test : IExternalCommand
+    { 
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+        TaskDialog.Show("Hello", "World");
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ VyssualsConnectorRevit/VyssualsElement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VyssualsConnectorRevit
+{
+    public struct VyssualsElement
+    {
+        public string Id;
+        public decimal Length;
+        public decimal Area;
+        public decimal Volume;
+        public Dictionary<string, object> Attributes;
+
+        public VyssualsElement(string id,
+            Dictionary<string, object> attributes,
+            decimal length = 0,
+            decimal area = 0,
+            decimal volume = 0)
+        {
+            this.Id = id;
+            this.Length = length;
+            this.Area = area;
+            this.Volume = volume;
+            this.Attributes = attributes;
+        }
+    }
+}
