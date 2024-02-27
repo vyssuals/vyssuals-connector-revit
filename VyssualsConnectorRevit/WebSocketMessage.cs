8adb19c13dd9b7e7050a5b4bcfd61d5029c2bfa6
Yskert Schindel <yskert.schindel@gmail.com>
2024-02-27 16:04:41 +0100

    add class WebSocketMessage

---
+++ VyssualsConnectorRevit/WebSocketMessage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Speckle.Newtonsoft.Json;
+
+namespace Vyssuals.ConnectorRevit
+{
+    public class WebSocketMessage
+    {
+        public string type { get; set; }
+        public string timestamp { get; set; }
+        public string version { get; set; }
+        public string sender { get; set; }
+        public string senderVersion { get; set; }
+        public string senderName { get; set; }
+        public Payload payload { get; set; }
+
+        public WebSocketMessage(string type, Payload payload)
+        {
+            this.type = type;
+            this.timestamp = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.fffffffZ");
+            this.version = "1.0";
+            this.sender = "Revit";
+            this.senderVersion = App.RevitVersion;
+            this.senderName = App.DocumentName;
+            this.payload = payload;
+        }
+
+        public string SerializeToJson()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+
+    public class Payload
+    {
+        public VyssualsElement[] data { get; set; }
+        public Dictionary<string, string> metadata { get; set; }
+    }
+}
