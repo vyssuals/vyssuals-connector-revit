8ca705094530392534a0e68327036ef7861101e1
Yskert Schindel <yskert.schindel@gmail.com>
2024-03-27 08:54:06 +0100

    Refactor JsonSerialization

--- VyssualsConnectorRevit/WebSocketMessage.cs
+++ VyssualsConnectorRevit/WebSocketMessage.cs
@@ -24,6 +24,7 @@ namespace Vyssuals.ConnectorRevit
         public string sender { get; set; }
         public string senderVersion { get; set; }
         public string senderName { get; set; }
+        [JsonConverter(typeof(PayloadConverter))]
         public Payload payload { get; set; }
 
         public WebSocketMessage(string timestamp, string type, Payload payload)
@@ -74,5 +75,56 @@ namespace Vyssuals.ConnectorRevit
         public string attributeName { get; set; }
         public string attributeValue { get; set; }
         public string[] ids { get; set; }
+    }
+
+    public class PayloadConverter : JsonConverter<Payload>
+    {
+        public override Payload Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            // Create a JsonDocument from the reader
+            JsonDocument doc = JsonDocument.ParseValue(ref reader);
+
+            try
+            {
+                // Determine the type of the payload based on the JSON data
+                // This will depend on the structure of your JSON data
+                // For example, you might check for the presence of certain properties
+                if (doc.RootElement.TryGetProperty("data", out _))
+                {
+                    // If the JSON data has a "data" property, assume it's a DataPayload
+                    return JsonSerializer.Deserialize<DataPayload>(doc.RootElement.GetRawText(), options);
+                }
+                else if (doc.RootElement.TryGetProperty("colors", out _))
+                {
+                    // If the JSON data has a "colors" property, assume it's a ColorPayload
+                    return JsonSerializer.Deserialize<ColorPayload>(doc.RootElement.GetRawText(), options);
+                }
+
+                // Add more checks for other Payload subclasses
+
+                // If none of the checks match, return null or throw an exception
+                return null;
+            }
+            finally
+            {
+                // Dispose the JsonDocument
+                doc.Dispose();
+            }
+        }
+
+        public override void Write(Utf8JsonWriter writer, Payload value, JsonSerializerOptions options)
+        {
+            // Implement your serialization logic here
+            // You can check the type of the value and call the appropriate serialization method
+            if (value is DataPayload)
+            {
+                JsonSerializer.Serialize(writer, value as DataPayload, options);
+            }
+            else if (value is ColorPayload)
+            {
+                JsonSerializer.Serialize(writer, value as ColorPayload, options);
+            }
+            // Add more else if blocks for other Payload subclasses
+        }
     }
 }
